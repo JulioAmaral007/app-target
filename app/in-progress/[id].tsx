74fd3571cc0ee778848dbe7f5c1927bdccf8bafe
Júlio Cézar <jczrpereira77@gmail.com>
2025-07-20 10:37:22 -0300

    feat(ui): add InProgress and Progress components to display financial progress

---
+++ app/in-progress/[id].tsx
@@ -0,0 +1,28 @@
+import { PageHeader } from '@/components/page-header'
+import { Progress } from '@/components/progress'
+import { useLocalSearchParams } from 'expo-router'
+import { View } from 'react-native'
+
+const details = {
+  current: 'R$ 580,00',
+  target: 'R$ 1.780,00',
+  percentage: 25,
+}
+
+export default function InProgress() {
+  const params = useLocalSearchParams<{ id: string }>()
+
+  return (
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch"
+        rightButton={{
+          icon: 'edit',
+          onPress: () => {},
+        }}
+      />
+
+      <Progress data={details} />
+    </View>
+  )
+}
