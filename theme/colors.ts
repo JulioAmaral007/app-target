f72e51f306af4135faa3926eb3787fa2df661407
Júlio Cézar <jczrpereira77@gmail.com>
2025-07-20 09:44:41 -0300

    chore(init): initialize project with base assets and configuration

---
+++ theme/colors.ts
@@ -0,0 +1,29 @@
+export const colors = {
+  blue: {
+    100: "#e1f5fe",
+    200: "#b3e5fc",
+    300: "#81d4fa", // Cor principal do azul claro no ícone
+    400: "#4fc3f7",
+    500: "#29b6f6",
+    800: "#0277bd",
+  },
+  gray: {
+    100: "#f5f5f5",
+    200: "#eeeeee",
+    300: "#e0e0e0",
+    400: "#bdbdbd",
+    500: "#9e9e9e",
+    600: "#757575",
+    800: "#3A404B", // Cor principal de fundo do ícone
+    900: "#2A2F3A", // Sombra mais escura do ícone
+  },
+  red: {
+    100: "#ffcdd2",
+    200: "#ef9a9a",
+    300: "#e57373", // Cor principal do vermelho/rosa no ícone
+    400: "#ef5350",
+    500: "#f44336",
+  },
+  white: "#ffffff", // Cor do texto "Target"
+  black: "#000000",
+};
