c8ade5abdfeedb8f0189896a8ada86f1ab4dd390
Júlio Cézar <jczrpereira77@gmail.com>
2025-07-20 10:30:26 -0300

    feat(ui): add PageHeader component for improved navigation and display of titles and subtitles

---
+++ app/target.tsx
@@ -0,0 +1,15 @@
+import { PageHeader } from '@/components/page-header'
+import { router } from 'expo-router'
+import { Button, View } from 'react-native'
+
+export default function Target() {
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
+        title="Meta"
+        subtitle="Economize para alcançar sua meta financeira."
+      />
+      <Button title="Voltar" onPress={() => router.back()} />
+    </View>
+  ) 
+}
