9a0886a2e04e4d82b6c5b28707c64fa36958f407
Júlio Cézar <jczrpereira77@gmail.com>
2025-07-20 10:08:49 -0300

    feat(home): add homepage header and update project dependencies

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { HomeHeader } from '@/components/home-header'
+import { View } from 'react-native'
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1 }}>
+     <HomeHeader data={{ total: 'R$ 2.680,00' }} />
+    </View>
+  )
+}
---
+++ components/home-header.tsx
@@ -0,0 +1,49 @@
+import { colors } from '@/theme/colors'
+import { fontFamily } from '@/theme/fontFamily'
+import { LinearGradient } from 'expo-linear-gradient'
+import { StyleSheet, Text, View } from 'react-native'
+
+export type HomeHeaderProps = {
+  total: string
+}
+
+type Props = {
+  data: HomeHeaderProps
+}
+
+export function HomeHeader({ data }: Props) {
+  return (
+<LinearGradient
+      colors={[colors.blue[500], colors.blue[800]]}
+      style={styles.container}
+    >
+      <View>
+        <Text style={styles.label}>Total que você possui</Text>
+        <Text style={styles.total}>{data.total}</Text>
+      </View>
+    </LinearGradient>
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 324,
+    paddingHorizontal: 24,
+    justifyContent: 'flex-end',
+    paddingBottom: 18,
+    gap: 24,
+  },
+  label: {
+    fontSize: 12,
+    color: colors.white,
+    fontFamily: fontFamily.regular,
+  },
+  total: {
+    fontSize: 32,
+    color: colors.white,
+    fontFamily: fontFamily.medium,
+  },
+})
