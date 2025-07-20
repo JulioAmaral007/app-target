4e8a4041045826b034e4c773614e3303d44b2769
Júlio Cézar <jczrpereira77@gmail.com>
2025-07-20 10:21:47 -0300

    feat(ui): add Target component to display financial goals on homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { HomeHeader } from '@/components/home-header'
+import { Target } from '@/components/target'
 import { View } from 'react-native'
 
 const summary = {
@@ -7,10 +8,20 @@ const summary = {
   output: { label: 'Saídas', value: '-R$ 883,65' },
 }
 
+const targets = [
+  {
+    name: 'Comprar uma cadeira ergonômica',
+    percentage: '75%',
+    current: '900,00',
+    target: '1.200,00',
+  },
+]
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
      <HomeHeader data={summary} />
+     <Target data={targets[0]} />
     </View>
   )
 }
